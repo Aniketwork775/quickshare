cbcd50df41ac74f2fd9ee37ec59db308598d8683
Aniketwork775 <aniket.bobby007@gmail.com>
2025-02-10 11:56:27 +0530

    Setting Up Componets and dependecies for quickShare

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
+import { FileListComponent } from './file-list/file-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'upload', component: UploadComponent },
+  { path: 'files', component: FileListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
