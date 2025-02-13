b89d60226ccd320bcf92aa974213fb937adebc94
Aniketwork775 <aniket.bobby007@gmail.com>
2025-02-13 10:32:18 +0530

    Run cleanup function automatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileService } from './services/file.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'quickshare';
+export class AppComponent implements OnInit {
+  constructor(private fileService: FileService) {}
+
+  ngOnInit() {
+    setInterval(() => {
+      this.fileService.cleanupExpiredFiles();
+    }, 60 * 60 * 1000); // ✅ Run every hour
+  }
 }
