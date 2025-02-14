91d7ce7ea696d0eec7202c2ec2e22d3285be30b4
Aniketwork775 <aniket.bobby007@gmail.com>
2025-02-14 15:21:21 +0530

    delete files on app closes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FileService } from '../services/file.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+  
+  constructor(private fileService: FileService){}
+
   upload:boolean=false;
   openFileDialog(){
     
@@ -13,5 +17,9 @@ export class HomeComponent {
 
   uploaded(){
     this.upload=true
+  }
+  
+  ngOnDestroy(): void {
+    this.fileService.cleanupondestroy();
   }
 }
